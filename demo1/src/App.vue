7855248662f4240ab41b0fbb9e036c29b9754f9b
DESKTOP-5TBM399\lx <534966250@qq.com>
2019-05-07 11:32:23 +0800

    修改了App

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -5,11 +5,21 @@
 </template>
 
 <script>
+  import {} from './js/resize'
 export default {
-  name: 'App'
+  name: 'App',
 }
 </script>
 
 <style>
-
+  html,body{
+    width: 100%;
+    height: 100%;
+  }
+   #app{
+     width: 100%;
+     height: 100%;
+     background-color:red;
+     font-siez:50px;
+   }
 </style>
