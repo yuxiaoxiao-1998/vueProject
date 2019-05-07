d1f02ed4a4295d4a07c97f9096758b7b35e48659
DESKTOP-5TBM399\lx <2432696110@qq.com>
2019-05-07 16:06:13 +0800

    插件最终

--- demo1/src/main.js
+++ demo1/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 Vue.config.productionTip = false
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
 
 new Vue({
   el: '#app',
