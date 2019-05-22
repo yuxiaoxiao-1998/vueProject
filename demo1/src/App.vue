869298d5a251a7c6e39a417c8773cb93971bb74b
DESKTOP-5TBM399\lx <2432696110@qq.com>
2019-05-22 10:32:09 +0800

    yxx路由动画

--- demo1/src/App.vue
+++ demo1/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition :name="transitionName">
+      <router-view class="child-view"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -9,20 +11,35 @@
   import YxCity from "./components/YxCity";
   import SearchCity from "./components/SearchCity";
   import Vue from 'vue';
-export default {
-  name: 'App',
-  components: {SearchCity, YxCity},
-  mounted () {
-    Vue.axios.get('https://elm.cangdu.org/promotion/v2/users/1/hongbaos?limit=20&offset=0').then((res)=>{
-      console.log(res.data);
-      // this.itemArr = res.data;
-      this.$store.state.packNum = res.data;
-    }).catch((err)=>{
-      console.log("请求错误",error);
-    });
-  },
+  export default {
+    name: 'App',
+    components: {SearchCity, YxCity},
+    mounted () {
+      Vue.axios.get('https://elm.cangdu.org/promotion/v2/users/1/hongbaos?limit=20&offset=0').then((res)=>{
+        console.log(res.data);
+        // this.itemArr = res.data;
+        this.$store.state.packNum = res.data;
+      }).catch((err)=>{
+        console.log("请求错误",error);
+      });
+    },
+    data () {
+      return {
+        transitionName: 'slide-left'
+      }
+    },
+    //监听路由的路径，可以通过不同的路径去选择不同的切换效果
+    watch: {
+      '$route' (to, from) {
+        if(to.path == '/'){
+          this.transitionName = 'slide-right';
+        }else{
+          this.transitionName = 'slide-left';
+        }
+      }
+    }
 
-}
+  }
 </script>
 
 <style>
@@ -33,8 +50,26 @@ export default {
     font-size: 20px;
     background: #F5F5F5;
   }
- *{
+  *{
     padding: 0;
     margin: 0;
- }
+  }
+  .child-view {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    transition: all .5s cubic-bezier(.55,0,.1,1);
+  }
+  .slide-left-enter, .slide-right-leave-active {
+    opacity: 0;
+    -webkit-transform: translate(30px, 0);
+    transform: translate(30px, 0);
+  }
+  .slide-left-leave-active, .slide-right-enter {
+    opacity: 0;
+    -webkit-transform: translate(-30px, 0);
+    transform: translate(-30px, 0);
+  }
 </style>
